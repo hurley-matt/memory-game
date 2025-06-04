f308838ab1cdcd2e6735eb0fe02bb45fe65b24cd
Matt Hurley <hurley04155@gmail.com>
2025-06-04 16:51:00 -0400

    Create divs with random pictures functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function Card(type) {
+  this.type = type;
+}
+
+const cardGenerator = Math.trunc(Math.random() * 16 + 1);
+let cardList = [];
+
+for (let i = 0; i < 16; i++) {
+  const cardNumber = cardGenerator;
+  if (!cardList.includes(cardNumber)) {
+    const card = new Card(cardNumber);
+    cardList.push(cardNumber);
+    console.log(card);
+  }
+}
